20f5da9eee4c517499044c6535e6c2aa6729f4e7
ovxon <67020288+ovxon@users.noreply.github.com>
2022-03-25 13:31:33 +0200

    epic stuff

---
+++ script3.js
@@ -0,0 +1,8 @@
+x = 5;
+var vards = document.getElementById(fname.value);
+var uzvards = document.getElementById(lname.value);
+var vecums = document.getElementById(age.value);
+var dzimums = document.getElementById(gender.value);
+function compileData(vards, uzvards, vecums, dzimums){
+    alert(vards + " " + uzvards + " " + vecums + " " + dzimums)
+}
