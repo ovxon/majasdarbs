ad5087062cc75f2a969f277865894970eec9c59a
ovxon <dclasher22@gmail.com>
2022-03-28 23:08:57 +0300

    epic 2

--- script4.js
+++ script4.js
@@ -1,10 +1,21 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 function checkData(){
-    var x_start = document.getElementById("x_start").value;
-    var y_start = document.getElementById("y_start").value;
-    var width = document.getElementById("width").value;
-    var height = document.getElementById("height").value;
+    var x_start = parseInt(document.getElementById("x_start").value);
+    var y_start = parseInt(document.getElementById("y_start").value);
+    var width = parseInt(document.getElementById("width").value);
+    var height = parseInt(document.getElementById("height").value);
     c.width = document.getElementById("c_width").value;
     c.height = document.getElementById("c_height").value;
+    if (x_start >= 0 && x_start < c.width && y_start >= 0 && y_start < c.height){
+        alert(x_start + " " + y_start + " " + width + " " + height + " " + (x_start + width) + " " + (y_start + height));
+        if ((x_start + width) <= c.width && (y_start + height) <= c.height){
+            ctx.fillStyle = "#FF0000";
+            ctx.fillRect(x_start, y_start, width, height);
+        } else {
+            alert("Platums vai augstums nav ievadīts pareizi, mēģini vēlreiz!");
+        }
+    } else {
+        alert("Kāda no sākuma koordinātām nav pareizi ievadīta, mēģini vēlreiz!");
+    }
 }
