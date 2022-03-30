29c51fdcb397b1b38dbc7f52c714dddcdd946846
ovxon <67020288+ovxon@users.noreply.github.com>
2022-03-30 14:20:55 +0300

    not so swag

--- script4.js
+++ script4.js
@@ -76,7 +76,7 @@ function checkData(){
     canvas = document.getElementById("c_color").value;
     c.width = document.getElementById("c_width").value;
     c.height = document.getElementById("c_height").value;
-    if (x_start >= 0 && x_start < c.width && y_start >= 0 && y_start < c.height){
+    if (x_start >= 0 && x_start <= c.width && y_start >= 0 && y_start <= c.height){
         if ((x_start + width) <= c.width && (y_start + height) <= c.height){
             if (document.getElementById("sq").checked && document.getElementById("r").checked){
                 drawRandomSquare()
@@ -89,8 +89,10 @@ function checkData(){
             }
         } else {
             alert("Platums vai augstums nav ievadīts pareizi, mēģini vēlreiz!");
+            return;
         }
     } else {
         alert("Kāda no sākuma koordinātām nav pareizi ievadīta, mēģini vēlreiz!");
+        return;
     }
 }
