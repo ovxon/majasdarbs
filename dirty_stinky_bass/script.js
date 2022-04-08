fd33eec9433f0e571c0da108f00bbf5df92dbe78
ovxon <dclasher22@gmail.com>
2022-04-08 14:21:00 +0300

    cawk

--- dirty_stinky_bass/script.js
+++ dirty_stinky_bass/script.js
@@ -2,3 +2,7 @@ var c1 = document.getElementById("myCanvas1");
 var ctx1 = c1.getContext("2d");
 var c2 = document.getElementById("myCanvas2");
 var ctx2 = c2.getContext("2d");
+
+function drawSquare() {
+    
+}
